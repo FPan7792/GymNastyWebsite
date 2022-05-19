6a98e3d9a8e25057207548ba6803d5aa5c70d116
Franck Panou <85650372+FPan7792@users.noreply.github.com>
2022-05-19 16:45:03 +0200

    fix jquery

--- app.ts
+++ app.ts
@@ -1,43 +1,43 @@
-// import $ from "jquery";
+$(() => {
+  const equipementImages = $(".item-equipement") as JQuery;
+  const modale = $(".equipements-modale-container") as JQuery;
+  const imageModale = $(".equipements-modale-bloc");
 
-const equipementImages = $(".item-equipement") as JQuery;
-const modale = $(".equipements-modale-container") as JQuery;
-const imageModale = $(".equipements-modale-bloc");
+  if (window.matchMedia("(min-width: 1000px)").matches) {
+    equipementImages.on("click", (e) => {
+      imageModale.attr(
+        "src",
+        `./ressources/img${e.target.dataset.equipement}-equip.jpg`
+      );
 
-if (window.matchMedia("(min-width: 1000px)").matches) {
-  equipementImages.on("click", (e) => {
-    imageModale.attr(
-      "src",
-      `./ressources/img${e.target.dataset.equipement}-equip.jpg`
-    );
+      modale.toggleClass("activeModale");
+    });
 
-    modale.toggleClass("activeModale");
+    modale.on("click", () => {
+      modale.toggleClass("activeModale");
+    });
+  }
+
+  const navbar = $(".navbar") as JQuery;
+
+  // window.addEventListener("scroll", () => {
+  //   if (window.scrollY > 0) {
+  //     navbar.addClass("navbar-anim-color");
+  //   } else navbar.removeClass("navbar-anim-color");
+
+  //   console.log("touhed");
+  // });
+  // console.log(window.scrollY);
+
+  $(window).on("scroll", () => {
+    if (window.scrollY > 0) {
+      navbar.addClass("navbar-anim-color");
+    } else navbar.removeClass("navbar-anim-color");
   });
 
-  modale.on("click", () => {
-    modale.toggleClass("activeModale");
-  });
-}
-
-const navbar = $(".navbar") as JQuery;
-
-// window.addEventListener("scroll", () => {
-//   if (window.scrollY > 0) {
-//     navbar.addClass("navbar-anim-color");
-//   } else navbar.removeClass("navbar-anim-color");
-
-//   console.log("touhed");
-// });
-// console.log(window.scrollY);
-
-// $(window).on("scroll", () => {
-//   if (window.scrollY > 0) {
-//     navbar.addClass("navbar-anim-color");
-//   } else navbar.removeClass("navbar-anim-color");
-// });
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 0) {
-    navbar.addClass("navbar-anim-color");
-  } else navbar.removeClass("navbar-anim-color");
+  // window.addEventListener("scroll", () => {
+  //   if (window.scrollY > 0) {
+  //     navbar.addClass("navbar-anim-color");
+  //   } else navbar.removeClass("navbar-anim-color");
+  // });
 });
