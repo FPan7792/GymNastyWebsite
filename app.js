c901e023eb8d2dc25c2762350c9f21cc328c1ef1
Franck Panou <85650372+FPan7792@users.noreply.github.com>
2022-05-19 17:10:58 +0200

    fix

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ const equipementImages = $(".item-equipement");
 const modale = $(".equipements-modale-container");
 const imageModale = $(".equipements-modale-bloc");
 if (window.matchMedia("(min-width: 1000px)").matches) {
-  equipementImages.on("click", (e) => {
-    imageModale.attr(
-      "src",
-      `./ressources/img${e.target.dataset.equipement}-equip.jpg`
-    );
-    modale.toggleClass("activeModale");
-  });
-  modale.on("click", () => {
-    modale.toggleClass("activeModale");
-  });
+    equipementImages.on("click", (e) => {
+        imageModale.attr("src", `./ressources/img${e.target.dataset.equipement}-equip.jpg`);
+        modale.toggleClass("activeModale");
+    });
+    modale.on("click", () => {
+        modale.toggleClass("activeModale");
+    });
 }
 const navbar = $(".navbar");
 // window.addEventListener("scroll", () => {
@@ -23,7 +20,9 @@ const navbar = $(".navbar");
 // });
 // console.log(window.scrollY);
 $(window).on("scroll", () => {
-  if (window.scrollY > 0) {
-    navbar.addClass("navbar-anim-color");
-  } else navbar.removeClass("navbar-anim-color");
+    if (window.scrollY > 0) {
+        navbar.addClass("navbar-anim-color");
+    }
+    else
+        navbar.removeClass("navbar-anim-color");
 });
